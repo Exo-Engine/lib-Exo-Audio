b6e25a1a3df4f5772a6d01bacfa5066bef5dbc85
Gaëtan Dezeiraud <gaetan@dezeiraud.com>
2020-06-28 00:38:44 +0200

    WIP

--- OpenAL/include/OALSound.h
+++ OpenAL/include/OALSound.h
@@ -24,12 +24,12 @@
 
 #pragma once
 
-#ifdef __linux__
-# include <AL/al.h>
-# include <AL/alc.h>
-#else
+#ifdef __APPLE__
 # include <OpenAL/al.h>
 # include <OpenAL/alc.h>
+#else
+# include <AL/al.h>
+# include <AL/alc.h>
 #endif
 
 #include "ISound.h"
